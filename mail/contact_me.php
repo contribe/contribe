8abd51878815ddc64092cddfe68c5ce4549ee63d
brian ranney <brian.ranney@contribe.com>
2015-07-09 18:52:29 +0200

    testing email function

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,10 +17,10 @@ $message = $_POST['message'];
 
 // Create the email and send the message
 $to = 'brian.ranney@gmail.com';
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
+$email_subject = "Website Contact Form";
+$email_body = "You have received a new message from your website contact form.";
+$headers = "From: brian.ranney@contribe.se"; 
+$headers .= "Reply-To";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
