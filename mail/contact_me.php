f597fb5b30a55a5025760f82bc4d743297bcb985
brian ranney <brian.ranney@contribe.com>
2015-07-09 19:01:33 +0200

    removed contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,10 +17,10 @@ $message = $_POST['message'];
 
 // Create the email and send the message
 $to = 'brian.ranney@gmail.com';
-$email_subject = "Website Contact Form";
-$email_body = "You have received a new message from your website contact form.";
-$headers = "From: brian.ranney@contribe.se"; 
-$headers .= "Reply-To";
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: info@contribe.se\n"; 
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
